ad3bc7b5d4890703f0920558dba5ab2d6a9c7968
Thiago Abreu <thiago.abreu@bitzar.solutions>
2022-09-12 20:20:49 -0300

    Added environment variables sample app

---
+++ 007-AppService_Basic/AppServiceBasic/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections;
+
+namespace AppServiceBasic.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        /// <summary>
+        /// Propoerty to list all the environment variables
+        /// </summary>
+        public IDictionary Variables { get; private set; } = new Dictionary<string, string>();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // Get all the variables of the environment to list
+            this.Variables = Environment.GetEnvironmentVariables();
+        }
+    }
+}
